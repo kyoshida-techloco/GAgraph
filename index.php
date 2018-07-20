4acdd026b296897a419f07445d43b9942a13f7f6
[kyoshida-techloco] <[kyoshida@techloco.co.jp]>
2018-07-20 16:22:31 +0900

    index.html追加

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>index</title>
+    </head>
+    <body>
+    <a href="http://www.google.com">
+        <button>google</button>
+    </a>
+    </body>
+</html>
