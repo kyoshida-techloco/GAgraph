5bdf0063be875058d70b761542ed6539bded2b78
[kyoshida-techloco] <[kyoshida@techloco.co.jp]>
2018-07-20 17:07:17 +0900

    元となる基本的なパーツ生成

---
+++ js/EmbedApi.js
@@ -0,0 +1,51 @@
+// Load Embed API
+(function(w,d,s,g,js,fs){
+  g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
+  js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
+  js.src='https://apis.google.com/js/platform.js';
+  fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
+}(window,document,'script'));
+
+gapi.analytics.ready(function(){
+    var CLIENT_ID="199814275824-gdriou3lp6kj3q168s0ua2utora8jfer.apps.googleusercontent.com";
+
+    gapi.analytics.auth.authorize({
+        container: 'auth-button',
+        clientid : CLIENT_ID,
+    });
+
+    var viewSelector = newgapi.analytics.ViewSelector({
+    container: 'view-selector'
+  });
+
+  // Step 5: Create the timeline chart.
+
+  var timeline = new gapi.analytics.googleCharts.DataChart({
+    reportType: 'ga',
+    query: {
+      'dimensions': 'ga:date',
+      'metrics': 'ga:sessions',
+      'start-date': '30daysAgo',
+      'end-date': 'yesterday',
+    },
+    chart: {
+      type: 'LINE',
+      container: 'timeline'
+    }
+  });
+
+  // Step 6: Hook up the components to work together.
+
+  gapi.analytics.auth.on('success', function(response) {
+    viewSelector.execute();
+  });
+
+  viewSelector.on('change', function(ids) {
+    var newIds = {
+      query: {
+        ids: ids
+      }
+    }
+    timeline.set(newIds).execute();
+  });
+});
